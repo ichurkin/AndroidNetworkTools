dc912c97ea87afc9cdd0cc393ce7de0611158b9a
Ivan Churkin <ivan.churkin@gmail.com>
2018-08-30 20:41:58 +0700

    Revert "Average time big fixed"
    This reverts commit d2c7b45bb2f1ed21010c14069df9b0928fe957e7.

--- library/src/main/java/com/stealthcopter/networktools/ping/PingTools.java
+++ library/src/main/java/com/stealthcopter/networktools/ping/PingTools.java
@@ -78,8 +78,7 @@ public class PingTools {
         try {
             long startTime = System.nanoTime();
             final boolean reached = ia.isReachable(null, pingOptions.getTimeToLive(), pingOptions.getTimeoutMillis());
-            //error /1e6f - nano to seconds = 1e9f
-            pingResult.timeTaken = (System.nanoTime() - startTime) / 1e9f;
+            pingResult.timeTaken = (System.nanoTime() - startTime) / 1e6f;
             pingResult.isReachable = reached;
             if (!reached) pingResult.error = "Timed Out";
         } catch (IOException e) {
